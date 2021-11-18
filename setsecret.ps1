dc6454f87ebc839c26089761834759007206d614
Freddy Kristiansen <freddyk@microsoft.com>
2021-11-18 10:23:14 +0100

    token

--- setsecret.ps1
+++ setsecret.ps1
@@ -2,11 +2,12 @@ $ErrorActionPreference = "stop"
 Set-StrictMode -Version 2.0
 
 gh version
+
 $token = 'Some strange token'
 $repository = $ENV:GITHUB_REPOSITORY
 
 Write-Host "authenticate with $token"
-$token | gh auth login --with-token
+"$token" | gh auth login --with-token
 
 Write-Host "write Secret"
 'NYSECRET' | gh secret set TESTSECRET --repo $repository
