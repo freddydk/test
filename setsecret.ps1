d1590e388e5a96c111f51e723f320f60cc69ee33
Freddy Kristiansen <freddyk@microsoft.com>
2021-11-18 16:01:54 +0100

    x

--- setsecret.ps1
+++ setsecret.ps1
@@ -12,6 +12,19 @@ $repository = $ENV:GITHUB_REPOSITORY
 $ENV:GITHUB_TOKEN = $token
 
 Write-Host "authenticate with $token"
+Write-Host 1
+gh auth login --with-token 1> $null
+Write-Host 2
+gh auth login --with-token 2> $null
+Write-Host 3
+gh auth login --with-token 3> $null
+Write-Host 4
+gh auth login --with-token 4> $null
+Write-Host 5
+gh auth login --with-token 5> $null
+Write-Host 6
 gh auth login --with-token 6> $null
+Write-Host 7
+gh auth login --with-token > $null
 gh secret set TESTSECRET -b $value --repo $repository
 Write-Host "done"
