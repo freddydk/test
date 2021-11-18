714933f5f6686f8a83ec18b36590e75bedda9163
Freddy Kristiansen <freddyk@microsoft.com>
2021-11-18 10:21:14 +0100

    x

--- setsecret.ps1
+++ setsecret.ps1
@@ -4,8 +4,10 @@ Set-StrictMode -Version 2.0
 gh version
 $token = 'Some strange token'
 $repository = $ENV:GITHUB_REPOSITORY
-Write-Host "authenticate"
+
+Write-Host "authenticate with $token"
 $token | gh auth login --with-token
+
 Write-Host "write Secret"
 'NYSECRET' | gh secret set TESTSECRET --repo $repository
 Write-Host "done"
